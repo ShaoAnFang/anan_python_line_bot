6ca5cf6d465ce51c2ad78ca6a06c0a3f331e63c2
Clark <clark.fang@habook.com.tw>
2017-07-25 14:56:26 +0800

    make it better

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def handle_message(event):
     line_bot_api.reply_message(
                                event.reply_token,
                                TextSendMessage(text=result))
-
+    return 0
 
 
 
