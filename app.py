b8163b1c7fd7e3a8da4d4307e6a0391e3f9b40fc
Clark <clark.fang@habook.com.tw>
2017-07-25 15:29:27 +0800

    make it better

--- app.py
+++ app.py
@@ -45,7 +45,9 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=result))
-        
+     else  line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
     
     
     #line_bot_api.reply_message(
