6630bf908cf653aa95311198dec4ada771df6563
紹安 <ilovet720419@gmail.com>
2017-07-26 23:52:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=result))
         
     if len(msg) > 50:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='有些文章自己看看就好 不要再轉貼廢文囉'))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='有些文章自己看看就好 廢文就不用再轉發了吧'))
     
     
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=msg))
