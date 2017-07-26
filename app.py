94d1138e2f7762b46bac89d9d281d1caca28eab1
紹安 <ilovet720419@gmail.com>
2017-07-27 00:10:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -130,10 +130,10 @@ def handle_message(event):
     
     
     if msg == '安安'
-        list = 'Hello Hello 你可以 \n' + '\n' + '1. 教我說話' + '安 你好=Hello World! \n' + '2. 輸入 股 2330 \n'
+        menulist = 'Hello Hello 你可以 \n' + '\n' + '1. 教我說話' + '安 你好=Hello World! \n' + '2. 輸入 股 2330 \n'
 
         
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=list))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=menulist))
     
     if msg == 'mongo':
         #dbResult = mongo()
