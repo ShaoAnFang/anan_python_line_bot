7c3cfdb83d17d8b0d366274aca5121b51b7bb6b9
紹安 <ilovet720419@gmail.com>
2017-07-27 09:45:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -110,22 +110,22 @@ def stock(stockNumber):
 def handle_message(event):
     msg = event.message.text
     
-    #if msg == '安安':
-    #    menulist = 'Hello Hello 你可以 \n' + '\n' + '1. 教我說話 \n' + '安 你好=Hello World! \n'
-    #    menulist += '2. 輸入 股 2330 \n' + '顯示該股票代碼的即時查詢 \n'
-    #    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=menulist))
+    if msg == '安安':
+        menulist = 'Hello Hello 你可以 \n' + '\n' + '1. 教我說話 \n' + '安 你好=Hello World! \n'
+        menulist += '2. 輸入 股 2330 \n' + '顯示該股票代碼的即時查詢 \n'
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=menulist))
     
-    #if msg == 'mongo':
-    #    #dbResult = mongo()
-    #    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='gogo mongo'))
+    if msg == 'mongo':
+        #dbResult = mongo()
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='gogo mongo'))
         
     if msg[0] == '股' and msg[1] == ' ' and len(msg) == 6:
         stockNumber = msg.split()[1]
         result = stock(stockNumber)
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=result))
         
-    #if len(msg) > 50:
-    #    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='有些文章自己看看就好 廢文就不用再轉發了吧'))
+    if len(msg) > 50:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='有些文章自己看看就好 廢文就不用再轉發了吧'))
     
     
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
