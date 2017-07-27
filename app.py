8643205b125b9c1b6a22f47b64ab9d10e80ac513
紹安 <ilovet720419@gmail.com>
2017-07-27 09:33:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,4 @@
-#!/usr/bin/env python3 
-# -*- coding: utf-8 -*-
-
-import pymongo
-import requests
-import json
-from bs4 import BeautifulSoup
 from flask import Flask, request, abort
-
-#from pymongo import MongoClient
-#uri = "mongodb://ilovet720419:720419@an-shard-00-00-cdgd9.mongodb.net:27017,an-shard-00-01-cdgd9.mongodb.net:27017,an-shard-00-02-cdgd9.mongodb.net:27017/?ssl=true&replicaSet=An-shard-0&authSource=admin"
-#client = MongoClient(uri)
-#db = client['An']
-#collect = db['test']
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -25,8 +12,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('E3V1P2J74V3qQ5VQsR0Au27E+NwBBlnh8r24mpP5vbkrogwj7PFroxNAKS9MU2iBeDMJiEFiaqe0SvKypYsoPcr70wVac/v4FJfXa1TwGPo0QeI1fkZcaejhJSz09aetC0TaMsblhNOorJaG4J/RlwdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('f2f133f2ba43194cf0e18503586023aa')
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
 
 @app.route("/callback", methods=['POST'])
@@ -46,108 +33,13 @@ def callback():
 
     return 'OK'
 
-#def mongo():
-    
-#    db = ''
-    
-#    for a in collect.find():
-#        db += a + '\n'
-    
-#    if not db:
-#        db += 'GG WP'
-    
-#    return db
 
-
-def stock(stockNumber):
-    url = 'https://www.google.com.hk/finance?q=TPE:'
-    url += stockNumber
-    header = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
-    res = requests.get(url,headers=header,verify=False)
-    res.encoding = 'utf-8'
-    soup = BeautifulSoup(res.text,'html.parser')
-    title = soup.find('h3')
-    title = title.text.strip()
-    #print(title)
-    
-    resultString = ''
-    resultString += title + '\n'
-    #現價
-    nowPrice = ''
-    for p in soup.select('.pr'):
-        #print(p.text)
-        nowPrice += p.text.strip()
-    
-    #漲跌
-    upDown = soup.select('.chr')
-    if not upDown :
-        upDown = soup.select('.chg')
-    uString = ''
-    for u in upDown:
-        #print(u.text.strip().encode('utf8'))
-        uString += u.text.strip()
-        #print(uString)
-    
-    key = []
-    for k in soup.select('.key'):
-        #print(k.text.strip().encode('utf8'))
-        key.append(k.text.strip())
-    
-    val = list()
-    for v in soup.select('.val'):
-        #print(v.text.strip().encode('utf8'))
-        val.append(v.text.strip())
-    
-    #現價
-    resultString += '-------------' + '\n'
-    resultString += '現價 ' + '\n' + nowPrice + '\n'
-    resultString += '-------------' + '\n'
-    #漲跌
-    resultString += '漲跌' + '\n' + uString + '\n'
-    resultString += '-------------' + '\n'
-    #每股盈餘
-    resultString += key[7] + '\n' + val[7] + '\n'
-    resultString += '-------------' + '\n'
-    #開盤
-    resultString += key[2]+ '\n' + val[2] + '\n'
-    resultString += '-------------' + '\n'
-    #範圍
-    resultString += key[0] + '\n' + val[0] + '\n'
-    resultString += '-------------' + '\n'
-    #52週
-    resultString += key[1] + '\n' + val[1] + '\n'
-    resultString += '-------------' + '\n'
-    #股息/收益
-    resultString += key[6] + '\n' + val[6] + '\n' resultString + '-------------' + '\n' + 'From Google stock'
-   
-    return resultString
-   
-  
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
-    
-    #if msg == '安安':
-    #    menulist = 'Hello Hello 你可以 \n' + '\n' + '1. 教我說話 \n' + '安 你好=Hello World! \n'
-    #    menulist += '2. 輸入 股 2330 \n' + '顯示該股票代碼的即時查詢 \n'
-    #    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=menulist))
-    
-    if msg == 'mongo':
-        #dbResult = mongo()
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='gogo mongo'))
-        
-    #if msg[0] == '股' and msg[1] == ' ' and len(msg) == 6:
-    #    stockNumber = msg.split()[1]
-    #    result = stock(stockNumber)
-    #    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=result))
-        
-    #if len(msg) > 50:
-    #    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='有些文章自己看看就好 廢文就不用再轉發了吧'))
-    
-    
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=msg))
-        
-  
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
 
 if __name__ == "__main__":
     app.run()
