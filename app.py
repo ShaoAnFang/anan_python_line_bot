7a5bdd08be9e879a9cab031f4d6bc8d0d44d28a2
紹安 <ilovet720419@gmail.com>
2017-07-27 10:19:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from bs4 import BeautifulSoup
 from flask import Flask, request, abort
 
 from pymongo import MongoClient
-uri = "mongodb://ilovet720419:720419@an-shard-00-00-cdgd9.mongodb.net:27017,an-shard-00-01-cdgd9.mongodb.net:27017,an-shard-00-02-cdgd9.mongodb.net:27017/?ssl=true&replicaSet=An-shard-0&authSource=admin"
 client = MongoClient(uri)
 db = client['An']
 collect = db['test']
