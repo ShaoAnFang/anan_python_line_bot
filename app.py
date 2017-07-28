51a3ac6126237faf0a2c94dbb30c2e3a50de8ee9
紹安 <ilovet720419@gmail.com>
2017-07-29 02:30:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -184,22 +184,22 @@ def handle_message(event):
                 text='Please select',
                 actions=[
                     PostbackTemplateAction(
-                    label='postback',
-                    text='postback text',
-                    data='action=buy&itemid=1'
-                ),
-                MessageTemplateAction(
-                    label='message',
-                    text='message text'
-                ),
-                URITemplateAction(
-                    label='uri',
-                    uri='http://example.com/'
-                )
-            ]
+                        label='postback',
+                        text='postback text',
+                        data='action=buy&itemid=1'
+                    ),
+                    MessageTemplateAction(
+                        label='message',
+                        text='message text'
+                    ),
+                    URITemplateAction(
+                        label='uri',
+                        uri='http://example.com/'
+                    )
+                ]
+            )
         )
-    )
-    line_bot_api.reply_message(event.reply_token, buttons_template)
+        line_bot_api.reply_message(event.reply_token, buttons_template)
         
     
     dbResult = firebaseQuery(msg)
