abf4a47a2ca80bb1845fbadf498c3d8c466f6186
紹安 <ilovet720419@gmail.com>
2017-07-29 02:13:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -178,6 +178,31 @@ def handle_message(event):
     if dbResult != '':
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=dbResult))
     
+    if msg == '電影':
+        buttons_template = TemplateSendMessage(
+            alt_text='正妹 template',
+            template=ButtonsTemplate(
+                title='選擇服務',
+                text='請選擇',
+                thumbnail_image_url='https://i.imgur.com/qKkE2bj.jpg',
+                actions=[
+                    MessageTemplateAction(
+                        label='PTT 表特版 近期大於 10 推的文章',
+                        text='PTT 表特版 近期大於 10 推的文章'
+                    ),
+                    MessageTemplateAction(
+                        label='來張 imgur 正妹圖片',
+                        text='來張 imgur 正妹圖片'
+                    ),
+                    MessageTemplateAction(
+                        label='隨便來張正妹圖片',
+                        text='隨便來張正妹圖片'
+                    )
+                ]
+            )
+        )
+    
+    
     
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
         
