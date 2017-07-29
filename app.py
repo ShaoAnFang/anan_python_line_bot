7e57d1a09d807f15001702f253e4520bcc0ec8a6
紹安 <ilovet720419@gmail.com>
2017-07-29 16:57:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ def handle_message(event):
         menulist = 'Hello 我是安安 你可以 \n' + '\n' + '1. 教我說話 \n' + '安 你好=Hello World! \n \n'
         menulist += '2. 輸入 股 2330 \n' + '顯示該股票代碼的即時查詢 \n'
 
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=events.source.userID)
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.source.userID)
       
         #line_bot_api.reply_message(event.reply_token,TextSendMessage(text=menulist))
 
