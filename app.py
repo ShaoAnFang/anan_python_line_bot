6bbddb3ff7289e62b4329547f9bcf68dc61dcfe1
紹安 <ilovet720419@gmail.com>
2017-07-29 12:12:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -153,13 +153,14 @@ def stock(stockNumber):
 def handle_message(event):
     msg = event.message.text
     
+    
     if msg == '安安':
         menulist = 'Hello 我是安安 你可以 \n' + '\n' + '1. 教我說話 \n' + '安 你好=Hello World! \n \n'
         menulist += '2. 輸入 股 2330 \n' + '顯示該股票代碼的即時查詢 \n'
         
         #gg = display_name + profile.user_id + picture_url
         
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=profile.user_id))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.source.userID))
       
         #line_bot_api.reply_message(event.reply_token,TextSendMessage(text=menulist))
 
