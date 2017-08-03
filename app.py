9d402fe80ad8b4974b3055e1452aa74d7289331c
紹安 <ilovet720419@gmail.com>
2017-08-04 00:24:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -235,8 +235,8 @@ def handle_message(event):
         #p = n + '\n \n' + p + '\n \n' + m
  
     if msg == '安安':
-        menulist = 'Hello 我是安安 你可以 \n' + '\n' + '1. 教我說話 \n' + '安 你好=Hello World! \n1.1 查詢教過的關鍵字 \n 查 AA\n1.2 刪除 教過的字 \n遺忘 AA \n\n'
-        menulist += '2. 輸入 天氣 台北 \n' + ' \n\n'
+        menulist = 'Hello 我是安安 你可以 \n' + '\n' + '1. 教我說話 \n' + '安 你好=Hello World! \n1.1 查詢教過的關鍵字 \n查 AA\n1.2 刪除 教過的字 \n遺忘 AA \n\n'
+        menulist += '2. 輸入 天氣 台北 \n\n'
         menulist += '3. 輸入 股 2330 \n' + '顯示該股票代碼的即時查詢 \n'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=menulist))
         
