c98001cbe4ab8aa965d560cc4d2e64c87b302764
紹安 <ilovet720419@gmail.com>
2017-08-04 00:09:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -165,7 +165,63 @@ def stock(stockNumber):
    
     return resultString
    
-  
+@app.route('/weather', methods=['GET'])
+def weather(ChooseCity):
+    cityDict = dict()
+cityDict = {'台北': 'Taipei_City', '新北': 'New_Taipei_City', '桃園': 'Taoyuan_City',
+          '台中': 'Taichung_City', '台南': 'Tainan_City', '高雄': 'Kaohsiung_City',
+          '基隆': 'Keelung_City', '新竹市': 'Hsinchu_City', '新竹縣': 'Hsinchu_County',
+          '苗栗市': 'Miaoli_County', '苗栗縣': 'Changhua_County', '南投': 'Nantou_County',
+          '雲林': 'Yunlin_County', '嘉義市': 'Chiayi_City', '嘉義縣': 'Chiayi_County',
+          '屏東': 'Pingtung_County', '宜蘭': 'Yilan_County', '花蓮': 'Hualien_County',
+          '台東': 'Taitung_County', '澎湖': 'Penghu_County','金門': 'Kinmen_County',
+          '連江': 'Lienchiang_County'}
+
+url = 'http://www.cwb.gov.tw/V7/forecast/taiwan/{}.htm'.format(cityDict[ChooseCity])
+#print(url)
+#header = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+#res = requests.get(url,headers=header,verify=False)
+res = requests.get(url,verify=False)
+res.encoding = 'utf-8'
+soup = BeautifulSoup(res.text,'html.parser')
+#print soup
+city = soup.select('.currentPage')[0].text.encode('utf8')
+#print(city)
+time = soup.select('.Issued')[0].text.encode('utf8')
+time = time.split(': ')[1]
+time = time.split(' ')[0]
+#print(time)
+
+imgTitle = soup.find_all('img')
+
+title = []
+for i in imgTitle: 
+    i = str(i).split('title="')[1]
+    i = str(i).split('"/>')[0]
+    #print i
+    title.append(i)
+
+content = soup.select('td')
+data = []
+for c in content:
+    c = c.text.strip('\n')
+    #print(c.encode('utf8'))
+    data.append(c)
+    
+resultString = ''
+resultString += '🌤 ' + city + '  '  + time + '\n\n' 
+resultString += '今晚至明晨 ' + str(data[0])  + '度\n' 
+resultString += title[0] + '  下雨機率 ' + str(data[3]) + '\n\n' 
+
+resultString += '明日白天' + str(data[4]) + ' 度\n'
+resultString += title[1] + '  下雨機率 ' + str(data[7]) + '\n\n' 
+
+resultString += '明日晚上' + str(data[8]) + ' 度\n'
+resultString += title[2] + '  下雨機率 ' + str(data[11])
+
+return resultString
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
@@ -234,7 +290,11 @@ def handle_message(event):
         fetchResult = firebaseFetch(string)
         result = '關鍵字 ' + string + ' 結果為: \n' + fetchResult
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=result))
-        
+    
+    if msg[0] == '天' msg[1] == '氣' msg[2] == ' ':
+        ChooseCity = msg.split('天氣 ')[1]
+        weatherResult = weather(ChooseCity)
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=weatherResult))
         
     if msg == 'temp':
         buttons_template_message = TemplateSendMessage(
