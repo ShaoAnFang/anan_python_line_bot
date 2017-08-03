b35695e476eeb804d7ba9abfac32cc2d976273ee
紹安 <ilovet720419@gmail.com>
2017-08-04 00:28:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ def weather(ChooseCity):
 
     resultString += '明日晚上' + str(data[8]) + ' 度\n'
     resultString += title[2] + '  下雨機率 ' + str(data[11]) + '\n'
-    resultString += '-交通部氣象局'
+
     return resultString
 
 
