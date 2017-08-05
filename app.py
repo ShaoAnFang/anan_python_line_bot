a302c15083ca4c3fd890c64c67031d7709d70a3b
紹安 <ilovet720419@gmail.com>
2017-08-05 17:52:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -371,7 +371,6 @@ def handle_message(event):
         
     if msg == 'temp':
         buttons_template_message = TemplateSendMessage(
-            {
             "type": "template",
             "altText": "this is a buttons template",
             "template": {
@@ -397,7 +396,6 @@ def handle_message(event):
                     }
                 ]
             }
-          }
         )
         line_bot_api.reply_message(event.reply_token, buttons_template_message)
         
