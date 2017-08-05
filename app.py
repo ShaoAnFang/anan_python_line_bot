15a0933e32e938f630812d11d42d02f162663a82
紹安 <ilovet720419@gmail.com>
2017-08-05 17:20:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -277,8 +277,8 @@ def handle_message(event):
     
     if event.message.type == 'sitcker' :
         sticker_message = StickerSendMessage(
-            package_id= event.message.packageId,
-            sticker_id= event.message.stickerId
+            package_id= event.message.package_id,
+            sticker_id= event.message.sticker_id
         )
         line_bot_api.reply_message(event.reply_token, sticker_message))
 
