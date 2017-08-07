f255fe09ab518a0346c5cc44d82f3827be3eff5d
紹安 <ilovet720419@gmail.com>
2017-08-07 23:25:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -314,7 +314,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=result))
         
     if len(msg) > 50:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='有些文章自己看看就好 這麼長也不會有人看吧'))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='未看先猜 __文'))
     
     if msg[0] == '安' and msg[1] == ' ':
         msg =  msg.strip('~!@#$%^&*()|"')
