e85f93027196be5722f54eaaf824e8fe7deb01f0
紹安 <ilovet720419@gmail.com>
2017-08-13 16:32:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -495,10 +495,12 @@ def handle_message(event):
 
     if dbResult != '' :
         #now = datetime.datetime.now()
-        global sendTime
-        if (time.time() - sendTime) > 3:
-            sendTime = datetime.datetime.now()
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=dbResult))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=dbResult))
+        
+        #global sendTime
+        #if (time.time() - sendTime) > 3:
+            #sendTime = datetime.datetime.now()
+            #line_bot_api.reply_message(event.reply_token,TextSendMessage(text=dbResult))
         
         
     #else:
