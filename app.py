f539ccf6bff16c6cb67cf1435b3de3b2d59e2e64
紹安 <ilovet720419@gmail.com>
2017-08-13 16:59:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -504,7 +504,7 @@ def handle_message(event):
         now = str(time.time()).split('.')[0]
         n = int(now)
         #line_bot_api.reply_message(event.reply_token,TextSendMessage(text=sendTimeStr))
-        if (n - s) > 3:
+        if (n - s) > 10:
             sendTime = time.time()
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text=dbResult))
         
