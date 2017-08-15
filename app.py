88d6cd02f0edf8feddf5f3df8719f3fe355a3a75
紹安 <ilovet720419@gmail.com>
2017-08-15 17:58:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -473,6 +473,9 @@ def handle_message(event):
                         )
                     ]
                 )
+              ]
+           )
+        )
         line_bot_api.reply_message(event.reply_token, carousel_template_message)
     
 
