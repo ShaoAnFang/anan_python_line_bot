a313dd8238141f5c37c6e0f1c11fbe7788d042ff
紹安 <ilovet720419@gmail.com>
2017-08-18 13:57:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -484,17 +484,6 @@ def handle_message(event):
                             uri=g[4]['poster_url']
                         )
                     ]
-                 ),
-                 CarouselColumn(
-                    thumbnail_image_url=g[4]['poster_url'],
-                    title=g[4]['ch_name'],
-                    text= g[4]['intro'],
-                    actions=[
-                        URITemplateAction(
-                            label='查看',
-                            uri=g[4]['poster_url']
-                        )
-                    ]
                  )
               ]
            )
