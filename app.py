89ea26dd4b7e12b441d81d5d5abdf186c3ffc59b
紹安 <ilovet720419@gmail.com>
2017-08-23 16:00:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -594,6 +594,28 @@ def handle_message(event):
                             uri=avgleResult[1]['video_url']
                         )
                     ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url=avgleResult[2]['preview_url'],
+                    title=avgleResult[2]['keyword'][:10],
+                    text= avgleResult[2]['title'][:10],
+                    actions=[
+                        URITemplateAction(
+                            label='查看',
+                            uri=avgleResult[2]['video_url']
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url=avgleResult[3]['preview_url'],
+                    title=avgleResult[3]['keyword'][:10],
+                    text= avgleResult[3]['title'][:10],
+                    actions=[
+                        URITemplateAction(
+                            label='查看',
+                            uri=avgleResult[3]['video_url']
+                        )
+                    ]
                 )
               ]
            )
