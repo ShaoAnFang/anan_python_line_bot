770e973a64c7885533c4f19b97106078eb783a93
紹安 <ilovet720419@gmail.com>
2017-08-23 14:19:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -602,7 +602,7 @@ def handle_message(event):
 
     if msg == '小電影':
         avgleResult = darkAnan()
-        asd = avgleResult[0]['title'] + ' ' + avgleResult[0]['preview_url'] +' '+ avgleResult[0]['keyword'] +' '+ avgleResult[0]['embedded_url']
+        asd = avgleResult[4]['title'] + ' ' + avgleResult[4]['preview_url'] +' '+ avgleResult[4]['keyword'] +' '+ avgleResult[4]['embedded_url']
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=asd))
 #         carousel_template_message = TemplateSendMessage(
 #         alt_text='小電影',
