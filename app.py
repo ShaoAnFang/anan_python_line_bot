ccaa5876c0f9e38195748d443a8c51bb9bd4ae2e
紹安 <ilovet720419@gmail.com>
2017-08-23 15:40:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -514,51 +514,51 @@ def handle_message(event):
                             uri=g[0]['poster_url']
                         )
                     ]
-                )
-#                 CarouselColumn(
-#                     thumbnail_image_url=g[1]['poster_url'],
-#                     title=g[1]['ch_name'],
-#                     text= g[1]['intro'],
-#                     actions=[
-#                         URITemplateAction(
-#                             label='查看',
-#                             uri=g[1]['poster_url']
-#                         )
-#                     ]
-#                 ),
-#                 CarouselColumn(
-#                     thumbnail_image_url=g[2]['poster_url'],
-#                     title=g[2]['ch_name'],
-#                     text= g[2]['intro'],
-#                     actions=[
-#                         URITemplateAction(
-#                             label='查看',
-#                             uri=g[2]['poster_url']
-#                         )
-#                     ]
-#                 ),
-#                 CarouselColumn(
-#                     thumbnail_image_url=g[3]['poster_url'],
-#                     title=g[3]['ch_name'],
-#                     text= g[3]['intro'],
-#                     actions=[
-#                         URITemplateAction(
-#                             label='查看',
-#                             uri=g[3]['poster_url']
-#                         )
-#                     ]
-#                 ),
-#                 CarouselColumn(
-#                     thumbnail_image_url=g[4]['poster_url'],
-#                     title=g[4]['ch_name'],
-#                     text= g[4]['intro'],
-#                     actions=[
-#                         URITemplateAction(
-#                             label='查看',
-#                             uri=g[4]['poster_url']
-#                         )
-#                     ]
-#                  )
+                ),
+                CarouselColumn(
+                    thumbnail_image_url=g[1]['poster_url'],
+                    title=g[1]['ch_name'],
+                    text= g[1]['intro'],
+                    actions=[
+                        URITemplateAction(
+                            label='查看',
+                            uri=g[1]['poster_url']
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url=g[2]['poster_url'],
+                    title=g[2]['ch_name'],
+                    text= g[2]['intro'],
+                    actions=[
+                        URITemplateAction(
+                            label='查看',
+                            uri=g[2]['poster_url']
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url=g[3]['poster_url'],
+                    title=g[3]['ch_name'],
+                    text= g[3]['intro'],
+                    actions=[
+                        URITemplateAction(
+                            label='查看',
+                            uri=g[3]['poster_url']
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url=g[4]['poster_url'],
+                    title=g[4]['ch_name'],
+                    text= g[4]['intro'],
+                    actions=[
+                        URITemplateAction(
+                            label='查看',
+                            uri=g[4]['poster_url']
+                        )
+                    ]
+                 )
               ]
            )
         )
@@ -583,51 +583,51 @@ def handle_message(event):
                             uri=avgleResult[0]['video_url']
                         )
                     ]
-                ),
-                CarouselColumn(
-                    thumbnail_image_url=avgleResult[1]['poster_url'],
-                    title=avgleResult[1]['keyword'],
-                    #text= avgleResult[1]['title'][:10],
-                    actions=[
-                        URITemplateAction(
-                            label='查看',
-                            uri=avgleResult[1]['video_url']
-                        )
-                    ]
-                ),
-                CarouselColumn(
-                    thumbnail_image_url=avgleResult[2]['preview_url'],
-                    title=avgleResult[2]['keyword'],
-                    #text= avgleResult[2]['title'][:10],
-                    actions=[
-                        URITemplateAction(
-                            label='查看',
-                            uri=avgleResult[2]['video_url']
-                        )
-                    ]
-                ),
-                CarouselColumn(
-                    thumbnail_image_url=avgleResult[3]['preview_url'],
-                    title=avgleResult[3]['keyword'],
-                    #text= avgleResult[3]['title'][:10],
-                    actions=[
-                        URITemplateAction(
-                            label='查看',
-                            uri=avgleResult[3]['video_url']
-                        )
-                    ]
-                ),
-                CarouselColumn(
-                    thumbnail_image_url=avgleResult[4]['preview_url'],
-                    title=avgleResult[4]['keyword'],
-                    #text= avgleResult[4]['title'][:10],
-                    actions=[
-                        URITemplateAction(
-                            label='查看',
-                            uri=avgleResult[4]['video_url']
-                        )
-                    ]
-                 )
+                )
+#                 CarouselColumn(
+#                     thumbnail_image_url=avgleResult[1]['poster_url'],
+#                     title=avgleResult[1]['keyword'],
+#                     #text= avgleResult[1]['title'][:10],
+#                     actions=[
+#                         URITemplateAction(
+#                             label='查看',
+#                             uri=avgleResult[1]['video_url']
+#                         )
+#                     ]
+#                 ),
+#                 CarouselColumn(
+#                     thumbnail_image_url=avgleResult[2]['preview_url'],
+#                     title=avgleResult[2]['keyword'],
+#                     #text= avgleResult[2]['title'][:10],
+#                     actions=[
+#                         URITemplateAction(
+#                             label='查看',
+#                             uri=avgleResult[2]['video_url']
+#                         )
+#                     ]
+#                 ),
+#                 CarouselColumn(
+#                     thumbnail_image_url=avgleResult[3]['preview_url'],
+#                     title=avgleResult[3]['keyword'],
+#                     #text= avgleResult[3]['title'][:10],
+#                     actions=[
+#                         URITemplateAction(
+#                             label='查看',
+#                             uri=avgleResult[3]['video_url']
+#                         )
+#                     ]
+#                 ),
+#                 CarouselColumn(
+#                     thumbnail_image_url=avgleResult[4]['preview_url'],
+#                     title=avgleResult[4]['keyword'],
+#                     #text= avgleResult[4]['title'][:10],
+#                     actions=[
+#                         URITemplateAction(
+#                             label='查看',
+#                             uri=avgleResult[4]['video_url']
+#                         )
+#                     ]
+#                  )
               ]
            )
         )
