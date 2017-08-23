2d6071fe6a0b5df08eafc52b931bc2c1c880aa78
紹安 <ilovet720419@gmail.com>
2017-08-23 15:29:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -567,22 +567,22 @@ def handle_message(event):
     
     if msg == '小電影':
         avgleResult = darkAnan()
-        asd = avgleResult[4]['title'][:10] + '\n' + avgleResult[4]['preview_url'] +'\n'+ avgleResult[4]['keyword'][:10] +'\n'+ avgleResult[4]['video_url']
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=asd))  
-#         carousel_template_message = TemplateSendMessage(
-#         alt_text='小電影',
-#         template=CarouselTemplate(
-#             columns=[
-#                 CarouselColumn(
-#                     thumbnail_image_url=avgleResult[0]['preview_url'],
-#                     title=avgleResult[0]['keyword'][:10],
-#                     text= avgleResult[0]['title'][:10],
-#                     actions=[
-#                         URITemplateAction(
-#                             label='查看',
-#                             uri=avgleResult[0]['video_url']
-#                         )
-#                     ]
+#         asd = avgleResult[4]['title'][:10] + '\n' + avgleResult[4]['preview_url'] +'\n'+ avgleResult[4]['keyword'][:10] +'\n'+ avgleResult[4]['video_url']
+#         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=asd))  
+        carousel_template_message = TemplateSendMessage(
+        alt_text='小電影',
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url=avgleResult[0]['preview_url'],
+                    title='水元恵梨香 となりの',
+                    text= avgleResult[0]['title'][:10],
+                    actions=[
+                        URITemplateAction(
+                            label='查看',
+                            uri=avgleResult[0]['video_url']
+                        )
+                    ]
 #                 ),
 #                 CarouselColumn(
 #                     thumbnail_image_url=avgleResult[1]['poster_url'],
@@ -626,12 +626,12 @@ def handle_message(event):
 #                             label='查看',
 #                             uri=avgleResult[4]['video_url']
 #                         )
-#                     ]
-#                  )
-#               ]
-#            )
-#         )
-#         line_bot_api.reply_message(event.reply_token, carousel_template_message)
+                    ]
+                 )
+              ]
+           )
+        )
+        line_bot_api.reply_message(event.reply_token, carousel_template_message)
     
     
     firebaseChatLog(msg)
