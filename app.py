4bd72da41f78296df549b802c676423bb4d0189b
紹安 <ilovet720419@gmail.com>
2017-08-25 15:17:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -373,6 +373,23 @@ def darkAnanQuery(name):
     return videoRandom
 
 
+def aime():
+    images = client.get_album_images('hLZwL')
+    #index = random.randint(0, len(images) - 1)
+    #url = images[index].link.replace('http', 'https')
+    albumResult = []
+    
+    for image in images:
+        imageDict = dict()
+        imageDict['imageLink'] = image.link.replace('http', 'https')
+        description = imgur.description.split('http')[0]
+        imageDict['title'] = title = description.split('$')[0]
+        imageDict['price'] = price = '$'+ description.split('$')[1]
+        imageDict['shopeeLink'] = image.description.encode('utf8').split('$')[1][3:]
+        albumResult.append(imageDict)
+        
+    return albumResult
+
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_message(event): 
     image_message = ImageSendMessage(
@@ -755,7 +772,74 @@ def handle_message(event):
             #cdTime = '紹安要我不能一直講話 \n還剩{}秒冷卻時間'.format(str(n - s))
             #line_bot_api.reply_message(event.reply_token,TextSendMessage(text=cdTime))
     
-
+    if msg == 'Aime' or 'aime' or 'AIME' :
+        
+        albumResult = aime()
+        #asd = avgleResult[4]['title'][:10] + '\n' + avgleResult[4]['preview_url'] +'\n'+ avgleResult[4]['keyword'][:10] +'\n'+ avgleResult[4]['video_url']
+        #line_bot_api.reply_message(event.reply_token,TextSendMessage(text=asd))
+        carousel_template_message = TemplateSendMessage(
+        alt_text='Aime',
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url=albumResult[0]['imageLink'],
+                    title=albumResult[0]['title'],
+                    text= albumResult[0]['price'],
+                    actions=[
+                        URITemplateAction(
+                            label='查看',
+                            uri=albumResult[0]['shopeeLink']
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url=avgleResult[1]['imageLink'],
+                    title=avgleResult[1]['title'],
+                    text= avgleResult[1]['price'],
+                    actions=[
+                        URITemplateAction(
+                            label='查看',
+                            uri=albumResult[1]['shopeeLink']
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url=albumResult[2]['imageLink'],
+                    title=albumResult[2]['title'],
+                    text= albumResult[2]['price'],
+                    actions=[
+                        URITemplateAction(
+                            label='查看',
+                            uri=albumResult[2]['shopeeLink']
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url=albumResult[3]['imageLink'],
+                    title=albumResult[3]['title'],
+                    text= albumResult[3]['price'],
+                    actions=[
+                        URITemplateAction(
+                            label='查看',
+                            uri=albumResult[3]['shopeeLink']
+                        )
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url=albumResult[4]['imageLink'],
+                    title=albumResult[4]['title'][:10],
+                    text= albumResult[4]['price'][:10],
+                    actions=[
+                        URITemplateAction(
+                            label='查看',
+                            uri=albumResult[4]['shopeeLink']
+                        )
+                    ]
+                )
+              ]
+           )
+        )
+        line_bot_api.reply_message(event.reply_token, carousel_template_message)
     
 
 
