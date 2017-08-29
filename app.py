2b4a25b96a416bf975b7fa5d26a62967ad5f2b7f
紹安 <ilovet720419@gmail.com>
2017-08-29 11:56:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -586,8 +586,9 @@ def handle_message(event):
         
                                 
     if msg == '電影':
-        if event.source.group_id is not None and event.source.group_id == 'C54f882fec4c5b8dc538b6d1cee5fc31f' :
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='GG'))
+        #if event.source.group_id == 'C54f882fec4c5b8dc538b6d1cee5fc31f' :
+        #    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='GG'))
+        
         g = get_movies()
         carousel_template_message = TemplateSendMessage(
         alt_text='電影',
