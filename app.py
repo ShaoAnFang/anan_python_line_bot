49b40540ec0511da12f30a46fb06efc700d8cf4b
紹安 <ilovet720419@gmail.com>
2017-09-06 00:28:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -417,7 +417,7 @@ def hospital():
     #res.encoding = 'big5'
     #res.encoding = 'utf8'
     soup = BeautifulSoup(res.text,'html.parser')
-    print soup
+    #print soup
 
     rows = soup.select('.tablecontent1')
     #print len(rows)
@@ -472,7 +472,8 @@ def handle_message(event):
         #p = n + '\n \n' + p + '\n \n' + m
     if msg == '三總':
         h = hospital()
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=h[0]))
+        g = h[0]
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=g))
         
     if msg == '安安':
         menulist = 'Hello 我是安安 你可以 \n' + '\n' + '1. 教我說話 \n' + '安 你好=Hello World! \n1.1 查詢教過的關鍵字 \n查 AA\n1.2 刪除 教過的字 \n遺忘 AA \n\n'
