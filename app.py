d67ececcdfb0a804a6223ced363b53d9afc52522
紹安 <ilovet720419@gmail.com>
2017-10-06 05:08:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -973,12 +973,13 @@ def handle_message(event):
         now = str(time.time()).split('.')[0]
         n = int(now)
         
-        if event.source.type !='group':
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=dbResult))
-        elif not event.source.group_id in quietArr :
-            #if (n - s) > 10 :
-                #sendTime = time.time()
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=dbResult))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=dbResult))
+#         if event.source.type !='group':
+#             line_bot_api.reply_message(event.reply_token,TextSendMessage(text=dbResult))
+#         elif not event.source.group_id in quietArr :
+#             #if (n - s) > 10 :
+#                 #sendTime = time.time()
+#             line_bot_api.reply_message(event.reply_token,TextSendMessage(text=dbResult))
 
 
 
