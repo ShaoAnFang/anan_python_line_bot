6b0788c385c04237c625023e658c5836f9814e2e
紹安 <ilovet720419@gmail.com>
2017-10-10 12:00:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -490,7 +490,7 @@ def handle_message(event):
             preview_image_url='https://imgur.com/VF0FSu5.jpg')
         line_bot_api.reply_message(event.reply_token, image_message) 
         
-    if msg == '吃懶' :
+    if msg.find('吃懶') != -1 :
         image_message = ImageSendMessage(
             original_content_url='https://imgur.com/5XILKP5.jpg',
             preview_image_url='https://imgur.com/5XILKP5.jpg')
