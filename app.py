3a9b0594490e8b5315e919586dd0e8e47c4c2bdc
紹安 <ilovet720419@gmail.com>
2017-11-01 11:50:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -933,23 +933,18 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, carousel_template_message)
         
     #firebaseChatLog(msg)
-    profile = line_bot_api.get_profile(event.source.user_id)
+    #profile = line_bot_api.get_profile(event.source.user_id)
     if event.source.type =='user' :
         #直接對機器人講
         z = '單獨(user_id):' + event.source.user_id
-        if profile.display_name is None:
-            firebaseChatLog(msg,'抓不到Name',z)  
-        else:
-            firebaseChatLog(msg,profile.display_name,z)   
-            
+        #firebaseChatLog(msg,profile.display_name,z)   
+        firebaseChatLog(msg,'',z)
+        
     elif event.source.type == 'group':
         #群組裡講
         z = '群組(group_id):' + event.source.group_id
-        if profile.display_name is None:
-            firebaseChatLog(msg,'抓不到Name',z)
-        else:
-            firebaseChatLog(msg,profile.display_name,z)
-            
+        #firebaseChatLog(msg,profile.display_name,z)
+        firebaseChatLog(msg,'',z)    
             
     if sticker(msg) != 'GG':
         if event.source.type !='group':
