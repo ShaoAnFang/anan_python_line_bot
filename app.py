a9d2be407ef25b0ec3e5f67a709fa6c1b07eb518
紹安 <ilovet720419@gmail.com>
2017-11-24 16:06:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -483,13 +483,25 @@ def hospital():
 @app.route('/wine', methods=['GET'])
 def wine():
 
+    f = {
+      "type": "service_account",
+      "project_id": "groupalbum-166505",
+      "private_key_id": "7e1b6365be1d48cec5716c208abbf7a7ce0f5095",
+      "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDDhNbntwtEgks5\nL5LXIaNwVjyt76L5P5YaG5eQSkoXpNHXuLoPf73yNuT1s7kTArf7sgPwaPess2+K\nDzKPnLc9Bkq728TC5D0lL4hzEKsDe/PdBqLfvoakNySEpJxXziFh/XjlND+BtkXD\nJe15xmuLc9bLZa6uQBWJdzTIoewSStfPMOoghAwLBYDAmlMzuzkK+690SiBUisb0\nOU6Sw96wsX86MO6wKIKBvgMTuegt0I6zVTei1LMsyjQplmFMuF0AUjfuQwo0BS0r\n3Kd0zdZs8OBBTitqfujKpaEVVVm4F6jopWsDqEJkT6LaaRfXRrPZ7p1Mxuv3c3TU\nSBeHzZwhAgMBAAECggEALGm1g4dB6PsL6V6UJYg/nxoHyJ8Bz0qRZzbTU/R8JijL\nfgw5P6IN3MJ5ehKsPNRGRMdoO6ksca+E68COBK9dtGAEg+3lITxFY3gLr8+eeTkj\n6RZD0z1tSko4YmUeGpf5X0/7sV0P/AASksS/0ASxk0jqBuP/LRfjjIlmlRirDnBJ\n46tv5de9XZlH1Twy0fG6+yFJGWd4uLQQm++O8xJ5loyDu7ZdWSYcJ9Bta4Ur41fb\n96tEvk7PnQ9dvGWLs00qIuSSZOigEIk5AuCxeKaMeR3SmdsK7jPuKQ5ZX3i/qcub\n6TIeVvtiZMLcAP5u+aFkTUVOKdE1dY1MEY3xbO/SXQKBgQDyZH28y+7udVwbl6mD\nbaBiqvQFKzHBVhE+x/dyx3Frm5r8ok30MKy375SdNligiGcSbLoLOIg5UJ31BTjf\nWb9f8WblQiHtpixMBbk5PghOwXulq95IOmYDsYR7Hx15KtsVPODBCO4w56RMgFAd\nK2KQa2Cg1nqr4fCCTLKd/AKdHwKBgQDOfrXvl1grKe77Fbb61KkaWqj+/9jCl2vo\nOxyl344BgEDBoXaW+YRmv4y4HXowritVP57URlT46Gq5V6gezGL6elTv6AGXYkRd\nFJGm+QUIj6WFSiQXGVlbdb1RIJrWysb1tb3T77nbAcN8Hgp3U2A+n/dSnO5sTLLG\nzV2sZbdevwKBgQDjoL131CfQZgQWoWl+VC//GL3KMNMr1dCiHZXigyufO2TkBOOi\nAyfgICx3Kvc14oKxCcv5B6Dd+jgsRjgvf4+54PCZMW69R4Vn6yQTfo68rvSYE3vO\nZpEwvL9GBGVgSX+uRRpoDSPqZ06izQjvK7QHHd+Di4dt7OM46h//Pw+RTwKBgBc8\nvR6UKnDZDDKnM+swKUN09lWT1wG25obAuC2WZbWXiDICCIVe2N5zKdPCRXDa+Ldk\nLGx46bEE/pWS3rFwkKbdQ1eoBR3TChxrZySiG0XmXFsOh9ctnBelvUM25xXKxe76\nn70M2h5iKWx7OPRKpqcFe2CJlm8LhobGr4bp/2OjAoGAZjANDFWt8sz1x4hqzdvj\nkKUGGlo66E00II8N1dN5oOfs7mlGBgX9X8uNqCQkewA5yx4lq1YkjiNrucsk8et0\n0lhVXm2k3Bae57gKoGOJ1iSlZpnLQD9sOJCavm39crCRrVtSsiy6vIsc+B3YV+jX\nNIuqgLPiszH2b+00HQMmLi0=\n-----END PRIVATE KEY-----\n",
+      "client_email": "testsheet@groupalbum-166505.iam.gserviceaccount.com",
+      "client_id": "117086094069483849270",
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+      "token_uri": "https://accounts.google.com/o/oauth2/token",
+      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/testsheet%40groupalbum-166505.iam.gserviceaccount.com"
+    }
     scope = ['https://spreadsheets.google.com/feeds']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(f, scope)
     client = gspread.authorize(creds)
     
     # Find a workbook by name and open the first sheet
     # Make sure you use the right name here.
-    sheet = client.open("wine").sheet1
+    sheet = client.open("酒吧巡迴清單").sheet1
     # Extract and print all of the values
     #list_of_hashes = sheet.get_all_records()
     list_of_hashes = sheet.get_all_values()
@@ -565,7 +577,7 @@ def handle_message(event):
         queryAllKeyAndValues.clear()
         queryAllKeyAndValues = firebase.get('/data',None)
 
-    if msg.find('哪喝') != -1:
+    if msg.find('哪喝'):
         w = wine()
         w += '\n\n這家如何呢!?'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=w))
