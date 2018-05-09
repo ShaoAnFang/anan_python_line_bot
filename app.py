7633048aa79b77da8c41a0565991355c2acf2ef4
紹安 <ilovet720419@gmail.com>
2018-05-09 09:07:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -603,26 +603,26 @@ def birthday(date):
             break
 
     #print(dictionary)
-    # if date != '沒填生日':
-    da = str(date)
-    d = da[0] + da[1] + '/' + da[2] + da[3]
-    if d in dictionary:
-        memberStr = ''
-        for m in dictionary[d]:
-            memberStr += m + ','
-        return memberStr
+    if date != '沒填生日':
+        da = str(date)
+        d = da[0] + da[1] + '/' + da[2] + da[3]
+        if d in dictionary:
+            memberStr = ''
+            for m in dictionary[d]:
+                memberStr += m + ','
+            return memberStr
+        else:
+            return '沒資料'
+
     else:
-        return '沒資料'
 
-    # else:
-
-    #     if date in dictionary:
-    #         memberStr = ''
-    #         for m in dictionary[date]:
-    #             memberStr += m + ','
-    #         return memberStr
-    #     else:
-    #         return '沒資料'
+        if date in dictionary:
+            memberStr = ''
+            for m in dictionary[date]:
+                memberStr += m + ','
+            return memberStr
+        else:
+            return '沒資料'
 
 # LocationMessage
 @handler.add(MessageEvent, message=LocationMessage)
@@ -658,9 +658,9 @@ def handle_message(event):
 
     #if event.source.group_id is not None:
     #    groupID = event.source.group_id 
-    # if msg == '沒填生日':
-    #     m = birthday(msg)
-    #     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=m))
+    if msg == '沒填生日':
+        m = birthday(msg)
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=m))
 
     if msg.find('生日') != -1:
         string = msg.split('生日')[1]
