d5585d9845c7a4c96598cb12888d1646cf527a7e
紹安 <ilovet720419@gmail.com>
2019-03-18 01:53:37 +0800

    Update Constellation.py

--- Module/Constellation.py
+++ Module/Constellation.py
@@ -6,7 +6,7 @@ def constellation(star):
     constellationDict = dict()
     constellationDict = {'牡羊': 'Aries', '金牛': 'Taurus', '雙子': 'Gemini','巨蟹': 'Cancer',
                             '獅子': 'Leo', '處女': 'Virgo', '天秤': 'Libra','天蠍': 'Scorpio', 
-                            '射手': 'Sagittarius', '魔羯': 'Capricorn','水瓶': 'Aquarius', '雙魚': 'Pisces'}
+                            '射手': 'Sagittarius', '魔羯': 'Capricorn', '摩羯':'Capricorn','水瓶': 'Aquarius', '雙魚': 'Pisces'}
 
     url = 'http://www.daily-zodiac.com/mobile/zodiac/{}'.format(constellationDict[star])
     res = requests.get(url,verify=False)
