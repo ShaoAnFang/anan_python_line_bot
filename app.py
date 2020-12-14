ab1907da3a1f3dd193fb555cc0f0f05647db7f9e
紹安 <ilovet720419@gmail.com>
2020-12-14 17:48:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -556,166 +556,22 @@ def handle_message(event):
         carousel_template_message = TemplateSend.aime(albumResult, msg)
         line_bot_api.reply_message(event.reply_token, carousel_template_message)
     if msg == 'flex':
-        flex_msg = FlexSendMessage(
-            alt_text = 'hello',
-            contents = { "type": "carousel",
-            "contents": [
-            {
-              "type": "bubble",
-              "hero": {
-                "type": "image",
-                "size": "full",
-                "aspectRatio": "20:13",
-                "aspectMode": "cover",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png"
-              },
-              "body": {
-                "type": "box",
-                "layout": "vertical",
-                "spacing": "sm",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "Arm Chair, White",
-                    "wrap": true,
-                    "weight": "bold",
-                    "size": "xl"
-                  },
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "$49",
-                        "wrap": true,
-                        "weight": "bold",
-                        "size": "xl",
-                        "flex": 0
-                      },
-                      {
-                        "type": "text",
-                        "text": ".99",
-                        "wrap": true,
-                        "weight": "bold",
-                        "size": "sm",
-                        "flex": 0
-                      }
-                    ]
-                  }
-                ]
-              },
-              "footer": {
-                "type": "box",
-                "layout": "vertical",
-                "spacing": "sm",
-                "contents": [
-                  {
-                    "type": "button",
-                    "style": "primary",
-                    "action": {
-                      "type": "uri",
-                      "label": "Add to Cart",
-                      "uri": "https://linecorp.com"
-                    }
-                  },
-                  {
-                    "type": "button",
-                    "action": {
-                      "type": "uri",
-                      "label": "Add to wishlist",
-                      "uri": "https://linecorp.com"
-                    }
-                  }
-                ]
-              }
-            },
-            {
-              "type": "bubble",
-              "hero": {
-                "type": "image",
-                "size": "full",
-                "aspectRatio": "20:13",
-                "aspectMode": "cover",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png"
-              },
-              "body": {
-                "type": "box",
-                "layout": "vertical",
-                "spacing": "sm",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "Metal Desk Lamp",
-                    "wrap": true,
-                    "weight": "bold",
-                    "size": "xl"
-                  },
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "flex": 1,
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "$11",
-                        "wrap": true,
-                        "weight": "bold",
-                        "size": "xl",
-                        "flex": 0
-                      },
-                      {
-                        "type": "text",
-                        "text": ".99",
-                        "wrap": true,
-                        "weight": "bold",
-                        "size": "sm",
-                        "flex": 0
-                      }
-                    ]
-                  },
-                  {
-                    "type": "text",
-                    "text": "Temporarily out of stock",
-                    "wrap": true,
-                    "size": "xxs",
-                    "margin": "md",
-                    "color": "#ff5551",
-                    "flex": 0
-                  }
-                ]
-              },
-              "footer": {
-                "type": "box",
-                "layout": "vertical",
-                "spacing": "sm",
-                "contents": [
-                  {
-                    "type": "button",
-                    "flex": 2,
-                    "style": "primary",
-                    "color": "#aaaaaa",
-                    "action": {
-                      "type": "uri",
-                      "label": "Add to Cart",
-                      "uri": "https://linecorp.com"
-                    }
-                  },
-                  {
-                    "type": "button",
-                    "action": {
-                      "type": "uri",
-                      "label": "Add to wish list",
-                      "uri": "https://linecorp.com"
-                    }
-                  }
-                ]
-              }
+        flex_message = FlexSendMessage(
+            alt_text='hello',
+            contents={
+                'type': 'bubble',
+                'direction': 'ltr',
+                'hero': {
+                    'type': 'image',
+                    'url': 'https://example.com/cafe.jpg',
+                    'size': 'full',
+                    'aspectRatio': '20:13',
+                    'aspectMode': 'cover',
+                    'action': { 'type': 'uri', 'uri': 'http://example.com', 'label': 'label' }
+                }
             }
-           ]
-          }
         )
-        line_bot_api.reply_message(event.reply_token, flex_msg)
+        line_bot_api.reply_message(event.reply_token, flex_message)
         
     #firebaseChatLog(msg)
     #profile = line_bot_api.get_profile(event.source.user_id)
