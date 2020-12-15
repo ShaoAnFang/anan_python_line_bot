548d0a2d44e9cfdc9941f71971d24698568337c1
紹安 <ilovet720419@gmail.com>
2020-12-15 17:37:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -530,7 +530,9 @@ def handle_message(event):
     if msg == 'zb':
         smzb_message = TemplateSend.smzb()
         line_bot_api.reply_message(event.reply_token, smzb_message)
-     
+    if msg == 'az':
+        test_message = TemplateSend.smzb()
+        line_bot_api.reply_message(event.reply_token, test_message)
     if msg == '小電影' or msg == 'AV':
         if event.source.type == 'group' and event.source.group_id == 'C54f882fec4c5b8dc538b6d1cee5fc31f':
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text=''))
