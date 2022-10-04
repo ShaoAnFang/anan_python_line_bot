f8baafc182bcbaa7a111a197733dc7a43ed5616e
紹安 <ilovet720419@gmail.com>
2022-10-04 12:05:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -559,10 +559,10 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, nba_message)
 
     if msg == '小電影' or msg == 'AV':
-        if event.source.type == 'group' and event.source.group_id == 'C54f882fec4c5b8dc538b6d1cee5fc31f':
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=''))
-        elif event.source.user_id == 'U2e046844ad61d32e4e091b2db7dbc53f':
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='GG WP'))
+#         if event.source.type == 'group' and event.source.group_id == 'C54f882fec4c5b8dc538b6d1cee5fc31f':
+#             line_bot_api.reply_message(event.reply_token,TextSendMessage(text=''))
+#         elif event.source.user_id == 'U2e046844ad61d32e4e091b2db7dbc53f':
+#             line_bot_api.reply_message(event.reply_token,TextSendMessage(text='GG WP'))
             
         avgleResult = darkAnan()
         carousel_template_message = TemplateSend.avgleSearch(avgleResult)
